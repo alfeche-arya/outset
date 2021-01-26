d31c635846b733e4978fb2260d0c74da0adbc50d
alanaalfeche <alanaalfeche@gmail.com>
2021-01-25 20:52:04 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import tmpBackground from './background.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={tmpBackground} />
+        <h1 style={{color: "mediumvioletred"}}> <b> <i> Coming soon... </i> </b> </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
