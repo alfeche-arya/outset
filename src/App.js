d15af18f9a96c36f4be656593ea295f170d2ea20
alanaalfeche <alanaalfeche@gmail.com>
2021-01-26 22:24:34 -0600

    Remove coming soon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import './App.css';
-import tmpBackground from './background.png';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={tmpBackground} alt="Temporary background for outset app"/>
-        <h1 style={{color: "mediumvioletred"}}> <b> <i> Coming soon... </i> </b> </h1>
-      </header>
-    </div>
+    "Outset App"
   );
 }
 
