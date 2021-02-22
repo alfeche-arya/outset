5062c2856a694a688bcba7a3830ad12d86899314
alanaalfeche <alanaalfeche@gmail.com>
2021-02-21 19:32:55 -0600

    Add simple crud in visions

--- src/components/Visions/Visions.js
+++ src/components/Visions/Visions.js
@@ -1,42 +1,100 @@
-import React from 'react';
+import React from "react";
 
-import Card from 'react-bootstrap/Card';
-import { Container } from 'react-bootstrap';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
-class Visions extends React.Component {
-    render() {
-        return (
-            <Container style={{margin: '80px auto', width: '50em'}}>
-            <Card>
-                <Card.Body>
-                <Card.Title>Vision Board #1</Card.Title>
-                <form>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Win the olympics" readOnly/>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Become the president"/>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Get married"/>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Placeholder #4"/>
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Placeholder #5"/>
-                    </div>
-                    <button type="button" class="btn btn-primary">Submit</button>
-                </form>        
+function Todo({ todo, index, completeTodo, removeTodo}) {
+    return (
+        <div style={{textDecoration: todo.isCompleted ? "line-through": ""}}>
+            <div style={{display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "white", margin: "20px", padding: "10px"}}>
+                {todo.text}
+                <div>
+                    <Button variant="success" onClick={() => completeTodo(index)} style={{marginRight: "10px"}}> Complete </Button>
+                    <Button variant="danger" onClick={() => removeTodo(index)}>x</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-                </Card.Body>
-                <Card.Footer style={{backgroundColor: "white"}}>
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-            </Container>
-        )
+function TodoForm({ addTodo }) {
+    const [value, setValue] = React.useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                classname="input"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                placeholder="Enter task here and press enter to add"
+                style={{width: "50%", height: "40px"}}
+            />
+        </form>
+    );
+}
+
+function Visions() {
+    const [todos, setTodos] = React.useState([
+        {
+            text: "Learn about React Hooks",
+            isCompleted: false
+        },
+        {   
+            text: "Make short ribs for dinner",
+            isCompleted: false
+        },
+        {
+            text: "Implement visions component",
+            isCompleted: false
+        }
+    ]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, { text}];
+        setTodos(newTodos);
     }
+
+    const completeTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = true;
+        setTodos(newTodos);
+    }
+
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    }
+    
+    return (
+        <Container style={{margin: "80px auto", width: "50em", backgroundColor: "whitesmoke"}}>
+            <div style={{padding: "20px 0px 0px 20px"}}>
+                <h4 style={{textAlign: "center"}}>My First Vision Board</h4>
+            </div>
+            <div style={{marginTop: "40px", textAlign: "center"}}>
+                <TodoForm addTodo={addTodo} />
+            </div>
+            <div style={{padding: "20px"}}>
+                {todos.map((todo, index) => (
+                    <Todo
+                        key={index}
+                        index={index}
+                        todo={todo}
+                        completeTodo={completeTodo}
+                        removeTodo={removeTodo}
+                    />
+                ))}
+            </div>
+        </Container>
+    );
 }
 
 export default Visions;
